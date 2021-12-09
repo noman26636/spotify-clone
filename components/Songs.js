2f21b791f1670d6311dcb54e17cbd2d40dd7c2ce
noman26636 <manu26636@gmail.com>
2021-12-09 11:25:25 +0500

    git commit

---
+++ components/Songs.js
@@ -0,0 +1,20 @@
+import { useRecoilValue } from "recoil"
+import { playlistState } from "../atom/playlistAtom"
+import Song from "./Song"
+
+
+function Songs() {
+
+    const playlist= useRecoilValue(playlistState)
+
+    return (
+        <div className="text-white p-8 flex flex-col space-y-2 pb-28">
+         {playlist?.tracks.items.map((track, order)=>(
+             <Song key={track.track.id} track={track} order={order}/>
+             
+         ))}
+        </div>
+    )
+}
+
+export default Songs
---
+++ pages/_middleware.js
@@ -0,0 +1,20 @@
+import { getToken } from "next-auth/jwt";
+import {NextResponse} from "next/server";
+
+export async function middleware(req){
+    // Token will exist if the user has logged in 
+           const token= await getToken({ req,secret: process.env.JWT_SECRET});
+
+           const { pathname }= req.nextUrl
+        // Allow the request if the following is true ...    
+        // 1. Its a request for next-auth session and provider fetching
+        // 2. if the token exists 
+        if(pathname.includes('/api/auth')|| token){
+            return NextResponse.next();
+        }
+        // Redirect them to login if they dont have token and are requested to a protected route
+         if(!token && pathname !== '/login'){
+             return NextResponse.redirect('/login');
+         }
+
+    }
